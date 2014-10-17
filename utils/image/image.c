df436edf131a28c8d85aadf74d9b85e5f24c6c61
Manuel HUEZ <manuel.huez@gmail.com>
2014-10-17 22:52:27 +0200

    Removed Loupou's work

--- utils/image/image.c
+++ utils/image/image.c
@@ -19,31 +19,6 @@ SDL_Surface *image_load(char *path)
 	return SDL_LoadBMP(path);
 }
 
-SDL_Surface *ScaleSurface(SDL_Surface *Surface, Uint16 Width, Uint16 Height)
-{
-	if(!Surface || !Width || !Height)
-		return 0;
-	 
-	SDL_Surface *_ret = SDL_CreateRGBSurface(Surface->flags, Width, Height,
-		Surface->format->BitsPerPixel, Surface->format->Rmask,
-		Surface->format->Gmask, Surface->format->Bmask, Surface->format->Amask);
- 
-	double _stretch_factor_x = (static_cast<double>(Width) /
-		static_cast<double>(Surface->w)),
-		_stretch_factor_y = (static_cast<double>(Height) /
-		static_cast<double>(Surface->h));
- 
-	for(Sint32 y = 0; y < Surface->h; y++)
-		for(Sint32 x = 0; x < Surface->w; x++)
-			for(Sint32 o_y = 0; o_y < _stretch_factor_y; ++o_y)
-				for(Sint32 o_x = 0; o_x < _stretch_factor_x; ++o_x)
-					DrawPixel(_ret, static_cast<Sint32>(_stretch_factor_x * x) +
-						o_x, static_cast<Sint32>(_stretch_factor_y * y) + o_y,
-						ReadPixel(Surface, x, y));
- 
-	return _ret;
-}
-
 SDL_Color image_getPixelColor(SDL_Surface *surface, unsigned x,
 	unsigned y)
 {
