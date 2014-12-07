ae8537e56edbedc9835c4ae07113fcb0afe73421
Manuel HUEZ <manuel.huez@gmail.com>
2014-12-07 21:51:12 +0100

    Trying to fix a compilation bug

--- utils/neuralNetwork/wordCorrector.h
+++ utils/neuralNetwork/wordCorrector.h
@@ -1,8 +1,12 @@
 
+#include <stdlib.h>
+#include <stdio.h>
+
 #ifndef UTILS_NEURALNETWORK_WORDCORRECTOR_
 #define UTILS_NEURALNETWORK_WORDCORRECTOR_
 
 	char *wordCorrector_correct(char *pathWordlist, char *word);
 	int wordCorrector_levenshtein(char *s1, char *s2);
+	size_t getline_perso(char **lineptr, size_t *n, FILE *stream);
 
 #endif
